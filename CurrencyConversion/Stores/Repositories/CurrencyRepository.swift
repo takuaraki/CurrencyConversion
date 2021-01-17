4e6ed7a2cca351d595d899bf5d4f75b8c4721b5a
takuaraki <takuaraki7@gmail.com>
2021-01-18 01:59:12 +0900

    impl CurrencyRepository

--- CurrencyConversion/Stores/Repositories/CurrencyRepository.swift
+++ CurrencyConversion/Stores/Repositories/CurrencyRepository.swift
@@ -6,6 +6,61 @@
 //
 
 import Foundation
+import RxSwift
 
-class CurrencyRepository {
+class CurrencyRepository: CurrencyRepositoryProtocol {
+    private let api: CurrencyAPIProtocol
+    private let db: CurrencyDBProtocol
+
+    init(
+        api: CurrencyAPIProtocol,
+        db: CurrencyDBProtocol
+    ) {
+        self.api = api
+        self.db = db
+    }
+
+    func getSupportedCurrencies() -> Single<[Currency]> {
+        return Single<[Currency]>.create { [db, api] observer in
+            if let cache = db.loadSupportedCurrencies() {
+                observer(.success(cache))
+                return Disposables.create()
+            }
+
+            let dto = api.getSupportedCurrencies()
+            let currencies = dto.currencies.map { code, name -> Currency in
+                return Currency(code: code, name: name)
+            }
+
+            db.saveSupportedCurrencies(currencies: currencies)
+
+            observer(.success(currencies))
+
+            return Disposables.create()
+        }
+    }
+    
+    func getConversionRates() -> Single<[ConversionRate]> {
+        return Single<[ConversionRate]>.create { [db, api] observer in
+            if let cache = db.loadConversionRates() {
+                observer(.success(cache))
+                return Disposables.create()
+            }
+
+            let dto = api.getConversionRates()
+            let rates = dto.quotes.map { codes, rate -> ConversionRate in
+                return ConversionRate(
+                    before: String(codes[..<codes.index(codes.startIndex, offsetBy: 3)]),
+                    after: String(codes[codes.index(codes.startIndex, offsetBy: 3)...]),
+                    rate: rate
+                )
+            }
+
+            db.saveConversionRates(exchangeRates: rates)
+
+            observer(.success(rates))
+
+            return Disposables.create()
+        }
+    }
 }
