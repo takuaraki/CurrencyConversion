ad93cbb52519f57c5688f078c69a4942cb6c32c8
takuaraki <takuaraki7@gmail.com>
2021-01-18 05:52:06 +0900

    show converted table

---
+++ CurrencyConversion/Views/ConvertedTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ConvertedTableViewCell.swift
+//  CurrencyConversion
+//
+//  Created by ArakiTaku on 2021/01/18.
+//
+
+import UIKit
+
+class ConvertedTableViewCell: UITableViewCell {
+    @IBOutlet weak var currencyCodeLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+}
--- CurrencyConversion/Views/CurrencyConversionViewController.swift
+++ CurrencyConversion/Views/CurrencyConversionViewController.swift
@@ -22,6 +22,14 @@ class CurrencyConversionViewController: UIViewController {
         titleForRow: { _, _, items, row, _ -> String? in
             return items[row]
         })
+    @IBOutlet weak var convertedListTableView: UITableView!
+    private lazy var convertedListDataSource: RxTableViewSectionedReloadDataSource<SectionModel<String, Money>> =
+        RxTableViewSectionedReloadDataSource<SectionModel<String, Money>>(configureCell: { dataSource, tableView, indexPath, money -> UITableViewCell in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ConvertedTableViewCell") as? ConvertedTableViewCell else { return UITableViewCell() }
+            cell.currencyCodeLabel.text = money.currency.code
+            cell.amountLabel.text = String(money.amount)
+            return cell
+        })
 
     private let disposeBag = DisposeBag()
 
@@ -31,6 +39,18 @@ class CurrencyConversionViewController: UIViewController {
         Observable.just(["One", "Two", "Tree"])
             .bind(to: currencyPickerView.rx.items(adapter: currencyPickerAdapter))
             .disposed(by: disposeBag)
+
+        Observable.just([
+            Money(amount: 1, currency: Currency(code: "USD", name: "USD")),
+            Money(amount: 103, currency: Currency(code: "JPY", name: "USD")),
+            Money(amount: 100, currency: Currency(code: "USD", name: "USD")),
+            Money(amount: 100, currency: Currency(code: "USD", name: "USD")),
+        ])
+        .map { moneys -> [SectionModel<String, Money>] in
+            return [SectionModel<String, Money>(model: "", items: moneys)]
+        }
+        .bind(to: convertedListTableView.rx.items(dataSource: convertedListDataSource))
+        .disposed(by: disposeBag)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
