05b1896bb5d51bf8536b4443a250d32cbf040abc
takuaraki <takuaraki7@gmail.com>
2021-01-18 04:38:28 +0900

    impl ViewModel

--- CurrencyConversion/ViewModels/CurrencyConversionViewModel.swift
+++ CurrencyConversion/ViewModels/CurrencyConversionViewModel.swift
@@ -6,6 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class CurrencyConversionViewModel {
+    // MARK: Inputs
+    let onViewDidLoad = PublishRelay<Void>()
+    let onCurrencySelected = PublishRelay<Currency>()
+    let onAmountChanged = PublishRelay<Float>()
+
+    // MARK: Outputs
+    private(set) lazy var currencies: Driver<[Currency]> = store.currencies
+    private(set) lazy var convertedList: Driver<[Money]> = store.convertedList
+
+    private let store: CurrencyConversionStoreProtocol
+
+    private let disposeBag = DisposeBag()
+    init(
+        store: CurrencyConversionStoreProtocol
+    ) {
+        self.store = store
+
+        onViewDidLoad.subscribe(onNext: { _ in
+            store.load()
+        }).disposed(by: disposeBag)
+
+        onCurrencySelected.subscribe(onNext: {
+            store.selectCurrency(currency: $0)
+        }).disposed(by: disposeBag)
+
+        onAmountChanged.subscribe(onNext: {
+            store.setAmount(amount: $0)
+        }).disposed(by: disposeBag)
+    }
 }
