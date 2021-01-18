941e67a3fbd1fecedb26fedfa587535b07069f4f
takuaraki <takuaraki7@gmail.com>
2021-01-18 20:19:42 +0900

    impl CurrencyDB

--- CurrencyConversion/Stores/Data/ConversionRate.swift
+++ CurrencyConversion/Stores/Data/ConversionRate.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ConversionRate: Equatable {
+struct ConversionRate: Equatable, Codable {
     let before: CurrencyCode
     let after: CurrencyCode
     let rate: Float
--- CurrencyConversion/Stores/Repositories/DB/CurrencyDB.swift
+++ CurrencyConversion/Stores/Repositories/DB/CurrencyDB.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-class CurrencyDB {
+class CurrencyDB: CurrencyDBProtocol {
+    func loadConversionRates() -> [ConversionRate]? {
+        if let saved = UserDefaults.standard.data(forKey: UserDefaultKey.conversionRates.rawValue) {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ConversionRate].self, from: saved) {
+                return decoded
+            }
+        }
+        return nil
+    }
+
+    func saveConversionRates(conversionRates: [ConversionRate]) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(conversionRates) {
+            UserDefaults.standard.set(encoded, forKey: UserDefaultKey.conversionRates.rawValue)
+        }
+    }
+}
+
+private enum UserDefaultKey: String {
+    case conversionRates
 }
